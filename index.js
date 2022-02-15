cb0afac8f65e854bc0c087704124f5964b8dacf9
Thomas Kinsella <thomasjkinsella4@gmail.com>
2022-02-15 13:09:03 -0500

    commit tree 236d6e4446eba739f9189c1e6084062fab45f817

---
+++ index.js
@@ -0,0 +1,35 @@
+function add(x, y) {
+    return x + y;
+};
+
+function subtract(x, y) {
+    return x - y;
+};
+
+function multiply(x, y) {
+    return x * y;
+};
+
+function divide(x, y) {
+    return x / y;
+};
+
+function increment(n) {
+    let num = n;
+    num++;
+    return num;
+};
+
+function decrement(n) {
+    let num = n;
+    num--;
+    return num;
+};
+
+function makeInt(n) {
+    return parseInt(n, 10);
+};
+
+function preserveDecimal(n) {
+    return parseFloat(n);
+};
